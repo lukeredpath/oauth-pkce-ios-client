bff16c2b8c507c343c2b28204d6f6c394ef4f3c2
Luke Redpath <luke@lukeredpath.co.uk>
2020-07-16 13:06:11 +0100

    Initial Commit

---
+++ OAuthTest/OAuthTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  OAuthTestApp.swift
+//  OAuthTest
+//
+//  Created by Luke Redpath on 16/07/2020.
+//
+
+import SwiftUI
+
+@main
+struct OAuthTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
